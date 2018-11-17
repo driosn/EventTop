0ac5b1d278543019888221bad536c4b9904044d4
driosn <davidsamuelrios07@gmail.com>
2018-11-17 12:53:56 -0400

    Cambios en all events.dart

--- lib/all_events.dart
+++ lib/all_events.dart
@@ -1,8 +1,55 @@
+import 'package:event_top/events_ink.dart';
 import 'package:flutter/material.dart';
 
-class All_Events extends StatelessWidget {
+class All_Events extends StatefulWidget {
+  @override
+  _All_EventsState createState() => _All_EventsState();
+}
+
+class _All_EventsState extends State<All_Events> {
+  /*List<DropdownMenuItem<int>> listDrop = [];
+
+  void loopData(){
+    listDrop.add(new DropdownMenuItem(
+      child: new Text('Web'),
+      value: 1
+    ));
+    listDrop.add(new DropdownMenuItem(
+        child: new Text('Mobile'),
+        value: 1
+    ));
+    listDrop.add(new DropdownMenuItem(
+        child: new Text('Artificial Intelligence'),
+        value: 1
+    ));
+  }*/
+
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue);
+    return Scaffold(
+        appBar: AppBar(
+          title: new Text('All Events'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.only(top: 15.0, bottom: 30.0, left: 15.0, right: 15.0),
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  height: 50.0,
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.red)),
+                ),
+                new Expanded(
+                  child: new ListView.builder(
+                    itemBuilder: (_, int index) => EventElement(),
+                    itemCount: 20,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
